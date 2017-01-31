dd700e5cab66a99dad03c79d9fc6c26403ce4bcc
OlaJFrick <info@culturehero.org>
2017-02-01 00:16:43 +0100

    Created Menu template & class with slide in animation

--- www/js/main.js
+++ www/js/main.js
@@ -14,21 +14,55 @@ var formHelpers = new FormHelpers();
 // Load html templates
 // (also see libs/template.jquery.js)
 $.loadTemplates([
-  'header',
-  'modal',
-  'navbar',
-  'restTestOutput',
-  'tableFromObject',
-  'formFromObject'
-],start);
+    'header',
+    'menu-slider',
+    'modal',
+    'navbar',
+    'restTestOutput',
+    'tableFromObject',
+    'formFromObject'
+], start);
 
 // Start the app
-function start(){
-  // Wait for DOM ready
-  $(()=>{
-    // Create the main navbar
-    new MainNavbar();
-    // Run the rest tests
-    new RestTests();
-  });
+function start() {
+    // Wait for DOM ready
+    $(() => {
+
+        // Create the main navbar
+        // new MainNavbar();
+
+        // Run the rest tests
+        // new RestTests();
+
+        // Create the menu
+        new Menu();
+
+        // slide in (animate) the menu 
+        menuSlideIn();
+
+    });
+}
+
+
+function menuSlideIn() {
+
+    $('.temp-x').click(function() {
+
+        // retrieve current state, initially undefined
+        var state = $(this).data('state');
+
+        // toggle the state - first click will make this "true"
+        state = !state;
+
+        if (state) {
+            $(".menu-slider").animate({ left: '-400px' }, 200);
+        } else {
+            $(".menu-slider").animate({ left: '10px' }, 200);
+        }
+
+        // put the state back
+        $(this).data('state', state);
+
+    });
+
 }
