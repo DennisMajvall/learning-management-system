b91eedd3106756b6eb4343379be0d2851c7f2db2
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-02-03 20:50:50 +0100

    edit student template now receives it's respective student in the constructor parameter. removed     <script src="js/classes/course-page.class.js"></script>

--- www/js/admin/admin-student-data.class.js
+++ www/js/admin/admin-student-data.class.js
@@ -1,19 +1,30 @@
 class AdminStudentData {
 
 	constructor() {
-		var someStudents;
+		var studentsDisplayed;
 
 		Student.find('', function(data, err) {
 			if(data.length > 20) {
-				someStudents = data.slice(data.length-20, data.length+1);
+				studentsDisplayed = data.slice(data.length-20, data.length+1);
 			} else {
-				someStudents = data;
+				studentsDisplayed = data;
 			}
 
-			$('body div.page-content').html('');
-			$('body div.page-content').template('admin-student-list', {students: someStudents});
+			$('body div.page-content').html('').template('admin-student-list', { students: studentsDisplayed });
+			
+			// Store the students in their respective data-attribute.
+			let studentIds = studentsDisplayed.map( student => student._id );
+			$('a.student-info').each((index, elem) => {
+				let me = $(elem);
+				let studentId = me.attr('student-id');
+				let foundIndex = studentIds.indexOf(studentId);
+				me.data('student', studentsDisplayed[foundIndex]);
+			});
 		});
     	
-    	$('body').on('click', 'a.student-info', ()=>{ new AdminStudentEditPage();});
+		$('body').on('click', 'a.student-info', function() {
+			let student = $(this).data('student');
+			new AdminStudentEditPage(student);
+		});
 	}
 }
--- www/js/admin/admin-student-edit-page.class.js
+++ www/js/admin/admin-student-edit-page.class.js
@@ -1,8 +1,8 @@
 class AdminStudentEditPage {
 
-	constructor() {
+	constructor(student) {
 
        	$('body div.page-content').html('');
-       	$('body div.page-content').template('admin-student-edit-page');
+		$('body div.page-content').template('admin-student-edit-page', { student: student });
     }
 }
