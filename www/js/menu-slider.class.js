f1a1c1379b93e631dc5ecd46093e3032b050b5e6
Victor Glimskog <victor.glimskog@gmail.com>
2017-02-08 09:33:48 +0100

    Menuslider and courses on frontpage now show the correct courses for the user logged in.

--- www/js/menu-slider.class.js
+++ www/js/menu-slider.class.js
@@ -1,20 +1,16 @@
 class MenuSlider {
 
-	constructor(role) {
+	constructor() {
 		// When we have login working we won't have to 'find' Students (or Teachers)
 		// and instead just populate the courses of the one logged in.
-		let schema = role == "Student" ? Student : Teacher;
-
 		let courseHashMap = {};
 		let that = this;
+		let currentUser = user;
 
-		schema.find('', function(data,err){
-			let testUser = data[0];
-			populateCourses(testUser.courses);
-		});
-		
+		populateCourses(currentUser.courses);
+
 		function populateCourses(courses) {
-			let coursesIds = courses.map( course => '"' + course._id + '"' );
+			let coursesIds = courses.map( course => '"' + course + '"' );
 			let queryString = 'find/{ _id: { $in: [' + coursesIds + '] } }';
 
 			Course.find(queryString, createTemplate);
