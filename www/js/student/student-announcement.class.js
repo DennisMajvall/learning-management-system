ad8394322a1be5177007b3c865fbe39fb8bbd299
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-02-08 10:17:21 +0100

    pictures can now be edited by admins, and pictures are displayed in most places

--- www/js/student/student-announcement.class.js
+++ www/js/student/student-announcement.class.js
@@ -5,14 +5,14 @@ class AnnouncementOnFrontpage {
 		//Posting all the announcements just for testing.
 		//Working on posting only the relevant announcements for the logged in student
 		//(with help of the studentID)
-		Student.find(studentID+"/getAnnouncements", function(announcementsToPrint){
+		Student.find(studentID+"/getAnnouncements", function(announcementsToPrint) {
 
 			announcementsToPrint = announcementsToPrint.returns;
 
 			let numberOfAnnouncements = Object.keys(announcementsToPrint).length;
 			let announcementsDone = 0;
 
-			announcementsToPrint.forEach(function(announcement){
+			announcementsToPrint.forEach(function(announcement) {
 
 				let coursesNames = "";
 				let lastAnnouncement = false;
@@ -22,23 +22,25 @@ class AnnouncementOnFrontpage {
 
 				announcementsDone++;
 
-				if(numberOfAnnouncements === announcementsDone){
+				if(numberOfAnnouncements === announcementsDone) {
 					numberOfCourses = Object.keys(announcement.courses).length;
 					lastAnnouncement = true;
 				}
 
-				Teacher.find(announcement.author, function(teacher){
+				Teacher.find(announcement.author, function(teacher) {
+
+					announcement.picture = teacher.picture;
 
 					announcement.author = teacher.firstname + " " + teacher.lastname;
 
-					announcement.courses.forEach(function(courseId){
+					announcement.courses.forEach(function(courseId) {
 
-						Course.find(courseId, function(course, doLast){
+						Course.find(courseId, function(course, doLast) {
 
 							coursesNames += " " + course.name;
 							coursesDone++;
 
-							if(lastAnnouncement === true && numberOfCourses === coursesDone){
+							if(lastAnnouncement === true && numberOfCourses === coursesDone) {
 
 								announcement.courses = coursesNames;
 
