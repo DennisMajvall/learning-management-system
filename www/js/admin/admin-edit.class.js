ccba292aa6c7eb583e7403a8c287b7e7f497157f
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-02-13 17:09:27 +0100

    added Create functionality for all schemas in Admin.

--- www/js/admin/admin-edit.class.js
+++ www/js/admin/admin-edit.class.js
@@ -2,7 +2,7 @@ class AdminEdit {
 
 	constructor(dbSchema, findItemFunc) {
 
-		$('body').on('click', 'button.delete-item', function() {
+		$('.edit-area').on('click', 'button.delete-item', function() {
 			let item = findItemFunc($(this));
 
 			dbSchema.delete(item._id, () => {
@@ -10,7 +10,7 @@ class AdminEdit {
 			});
 		});
 
-		$('body').on('click', 'button.save-item', function() {
+		$('.edit-area').on('click', 'button.save-item', function() {
 			let item = findItemFunc($(this));
 			let objectToSave = Object.assign({}, item);
 
@@ -24,7 +24,7 @@ class AdminEdit {
 			});
 		});
 
-		$('body').on('keyup', '[bind-key]', function() {
+		$('.edit-area').on('keyup', '[bind-key]', function() {
 			let item = findItemFunc($(this));
 			let key = $(this).attr('bind-key');
 
@@ -39,23 +39,23 @@ class AdminEdit {
 		// remove marked items
 		$('.edit-area').on('click', 'button.remove-items', function() {
 			let itemsToRemove = $('.edit-area a.active');
-			
+
 			// use .edit-buttons as a referens point to get course
 			let mainItem = findItemFunc($('.edit-area .edit-buttons'));
 
-			itemsToRemove.map(function(){
+			itemsToRemove.map(function() {
 				let itemCategory = $(this).closest('[item-type]').attr('item-type');
 				let itemId = $(this).attr('list-item-id');
 
 				let objectToSave = Object.assign({}, item);
 
-				if(itemCategory === "students"){
-				
+				if(itemCategory === "students") {
+
 					console.log(itemId);
 					// Course.update(mainItem._id, objectToSave, () => {
 					// 	location.reload();
 					// });
-				} else if(itemCategory === "teachers"){
+				} else if(itemCategory === "teachers") {
 
 					console.log(itemId);
 					// Course.update(mainItem._id, objectToSave, () => {
