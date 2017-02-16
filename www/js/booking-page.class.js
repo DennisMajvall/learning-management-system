4009b3009e356bff90b97cbd4b0925d7ff6de9b3
Victor Glimskog <victor.glimskog@gmail.com>
2017-02-16 14:32:45 +0100

    Lots of code to get the template for the booking page done. HTML and Less

--- www/js/booking-page.class.js
+++ www/js/booking-page.class.js
@@ -1,5 +1,14 @@
 class BookingPage{
 	constructor(){
-		$('.page-content').template('booking-page',{});
+		var fakeRooms = [];
+		for(var i=0; i<5; i++){
+			fakeRooms[i]={
+				name: "Room " + (i+1),
+				type: "classroom",
+				seats: 50
+			};
+		}
+		console.log(fakeRooms);
+		$('.page-content').template('booking-page',{rooms: fakeRooms});
 	}
 }
