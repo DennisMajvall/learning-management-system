a1dd8700a2af2a3d18441f5ab72f3f8e799be475
Martin Lennervald <martin.lennervald@gmail.com>
2017-02-23 15:44:21 +0100

    Working, but no styles and no password check

--- www/js/profile.class.js
+++ www/js/profile.class.js
@@ -1,24 +1,31 @@
 class Profile {
   constructor () {
+    let that = this;
     this.currentUser = user;
     this.dbSchema = this.currentUser.role === "Student" ? Student : Teacher;
-
-    // for test
-    console.log('currentUser.role: ', this.currentUser.role );
-    console.log('dbSchema: ', this.dbSchema);
 
     // Load template for profile page
     this.renderTemplate();
 
     // Register clicks in save-btn
     $('.profile-page-container').on('click', '.save-profile', function(){
+      // get values from form
+      that.currentUser.firstname = $('.profile-page-container').find('input[name="firstname"]').val();
+      that.currentUser.lastname = $('.profile-page-container').find('input[name="lastname"]').val();
+      that.currentUser.phonenumber = $('.profile-page-container').find('input[name="phonenumber"]').val();
+      that.currentUser.picture = $('.profile-page-container').find('input[name="picture"]').val();
 
+      user = Object.assign({}, that.currentUser);
+      that.dbSchema.update(user._id, that.currentUser, function(){
+        location.reload();
+      });
     });
 
   }
 
   renderTemplate (){
     let currentUser = this.currentUser;
+
 
     // Clear the page
     $('.student-alert-container').empty();
