bcd770d16883c7873a819a23e93823a5a7eb2353
Martin Lennervald <martin.lennervald@gmail.com>
2017-02-23 14:48:56 +0100

    Fix for rendering profile page template

--- www/js/profile.class.js
+++ www/js/profile.class.js
@@ -2,25 +2,32 @@ class Profile {
   constructor () {
     this.currentUser = user;
     this.dbSchema = this.currentUser.role === "Student" ? Student : Teacher;
-    console.log('dbSchema: ',this.dbSchema);
 
+    // for test
+    console.log('currentUser.role: ', this.currentUser.role );
+    console.log('dbSchema: ', this.dbSchema);
+
+    // Load template for profile page
     this.renderTemplate();
+
+    // Register clicks in save-btn
+    $('.profile-page-container').on('click', '.save-profile', function(){
+
+    });
+
   }
 
   renderTemplate (){
     let currentUser = this.currentUser;
-    // Clear the page
-    $('.page-content').children().empty();
-    // Add html template for profile
-    $('.page-content').template('profile', {currentUser})
-  }
 
-  getDbSchema(){
-    if (this.currentUser.role == 'Student'){
-      return Student;
-    }
-    else
-      return Teacher;
+    // Clear the page
+    $('.student-alert-container').empty();
+    $('.student-announcement-container').empty();
+    $('.teacher-messages-container').empty();
+    $('.front-course-container').empty();
+
+    // Add html template for profile
+    $('.profile-page-container').empty().template('profile', {currentUser})
   }
 }
 
