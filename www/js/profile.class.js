cb82b8a19d1a6f2680726c95d670e6132e8d4a17
Martin Lennervald <martin.lennervald@gmail.com>
2017-02-23 14:19:30 +0100

    added structure for profile page

--- www/js/profile.class.js
+++ www/js/profile.class.js
@@ -1,5 +1,36 @@
 class Profile {
   constructor () {
+    this.currentUser = user;
+    this.dbSchema = this.currentUser.role === "Student" ? Student : Teacher;
+    console.log('dbSchema: ',this.dbSchema);
 
+    this.renderTemplate();
+  }
+
+  renderTemplate (){
+    let currentUser = this.currentUser;
+    // Clear the page
+    $('.page-content').children().empty();
+    // Add html template for profile
+    $('.page-content').template('profile', {currentUser})
+  }
+
+  getDbSchema(){
+    if (this.currentUser.role == 'Student'){
+      return Student;
+    }
+    else
+      return Teacher;
   }
 }
+
+
+
+
+
+// TODO
+/*
+  Rendera template
+  Fånga clicks på save knapp
+  spara mot db
+*/
