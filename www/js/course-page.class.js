09eb106e38931edf869f0f33bd1255580853eab6
Hampus Johansson <hampusjohansson87@gmail.com>
2017-03-02 13:14:30 +0100

    add user to course.

--- www/js/course-page.class.js
+++ www/js/course-page.class.js
@@ -19,8 +19,11 @@ class CoursePage{
 				role: user.role
 			});
 
+
+			$('.add-student').on('click', addStudent);
+
 			courseObj = course;
-			
+
 			announcementQuery = 'find/{ courses: { $in: ["' + courseObj._id + '"] } }';
 
 			Announcement.find(announcementQuery, announcementsFound);
@@ -52,6 +55,54 @@ class CoursePage{
 
 			that.removeById(id, courseObj, that, this);
 		});
+
+		function addStudent(){
+			// Get the username from the textbox
+			let username = $('.add-student-input').val();
+
+			// Find student in database
+			Student.find('find/{ username: "' + username + '" }', function(result) {
+
+				// If student exists in the database
+				if (result.length) {
+					let student = result[0];
+					let studentsInCourse = courseObj.students;
+					let studentIsAlreadyInCourse = false;
+
+					// Loop through the course's students and check if
+					// a student with the specified username is already in there.
+					studentsInCourse.forEach(function(obj) {
+						if(obj.username == username) {
+							studentIsAlreadyInCourse = true;
+						}
+					});
+
+					if (studentIsAlreadyInCourse) {
+						// show error msg, student already in course
+					} else {
+						// Add student to course.
+						studentsInCourse.push(student);
+
+						// Add course to student.
+						student.courses.push(courseObj);
+
+						// Update the course in the database
+						Course.update(courseObj._id, { students: studentsInCourse }, function(){
+
+							// Update the student in the database.
+							Student.update(student._id, { courses: student.courses }, function(){
+
+								// Reload the page.
+								location.reload();
+							});
+
+						})
+					}
+				} else {
+					// show error msg, wrong username
+				}
+			});
+		}
 	}
 
 	removeById(id, mainItem, that, domThis) {
